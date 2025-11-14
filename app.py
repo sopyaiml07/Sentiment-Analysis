9ff74f1a86ec4faa17ca258c3ec0f44d52f56445
SONAL BHATT <sonalbhatt0703@gmail.com>
2025-11-14 15:08:18 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask,render_template, request
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+import nltk
+nltk.download('vader_lexicon')
+app=Flask(__name__)
+
+@app.route('/', methods=["GET","POST"])
+def main():
+    if request.method=="POST":
+        inp=request.form.get("inp")
+        sid=SentimentIntensityAnalyzer()
+        score= sid.polarity_scores(inp)
+        if score["neg"] !=0:
+            return render_template('home.html', message="Negative☹️")
+        else:
+            return render_template('home.html', message="Positive😊")
+    return render_template('home.html')
